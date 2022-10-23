3aaa4dd395c164d971f3dc3d2f280a4d1c7525fc
Suprememajor <ndipnobertetta@gmail.com>
2022-10-23 04:49:31 +0100

    Create RegistrationServer

---
+++ src/test/java/com/supremecorp/serviceregistrationserver/ServiceRegistrationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supremecorp.serviceregistrationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
